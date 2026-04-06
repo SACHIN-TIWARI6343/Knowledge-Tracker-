41635c5944a75ee997c495809f7d54db244d8008
Sachin Tiwari <tiwarisachin6343@gmail.com>
2026-04-06 15:53:22 +0530

    full satck complete

---
+++ demo/demo/src/main/java/com/Knowledge/demo/controller/NoteController.java
@@ -0,0 +1,60 @@
+package com.Knowledge.demo.controller;
+
+import com.Knowledge.demo.entity.Note;
+import com.Knowledge.demo.service.NoteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional; // use to avoid null pointer
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/notes") // base url
+public class NoteController {
+
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    // Create Note
+    @PostMapping
+    public Note createNote(@RequestBody Note note) {
+        return noteService.createNote(note);
+    }
+
+    // Get All Notes
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteService.getAllNotes();
+    }
+
+    // Get Note by ID
+    @GetMapping("/{id}")
+    public Optional<Note> getNoteById(@PathVariable Long id) {
+        return noteService.getNoteById(id);
+    }
+
+    // Update Note
+    @PutMapping("/{id}")
+    public Note updateNote(@PathVariable Long id, @RequestBody Note note) {
+        return noteService.updateNote(id, note);
+    }
+
+    // Delete Note
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable Long id) {
+        noteService.deleteNote(id);
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Note> getNotesByCategory(@PathVariable String category) {
+        return noteService.getNotesByCategory(category);
+    }
+
+    @GetMapping("/status/{status}")
+    public List<Note> getNotesByStatus(@PathVariable String status) {
+        return noteService.getNotesByStatus(status);
+    }
+}
---
+++ demo/demo/src/main/java/com/Knowledge/demo/entity/Note.java
@@ -0,0 +1,62 @@
+package com.Knowledge.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity //  it tells JPA/Hibernate to create  a database table from this class
+public class Note {
+
+    @Id // to define primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // so that database automatically id generate kare
+    private Long id;
+
+    private String title;
+    private String description;
+    private String category;
+    private String status;
+    private LocalDateTime createdAt;
+
+    public Note() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
